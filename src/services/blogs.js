b850cfc00e808c9c6e4d82d82c602fb8dd7ef221
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-03-02 10:05:54 -0300

    Edit blogs service: now supports users' token

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,28 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default { getAll }
+const create = newNote => {
+  const config = {
+    headers: {
+      Authorization: token
+    }
+  }
+
+  const request = axios.post(baseUrl, newNote, config)
+
+  return request.then(response =>
+    console.log('New blog added: ', response.data))
+}
+
+export default { getAll, create, setToken }
