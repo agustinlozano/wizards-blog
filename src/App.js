455b98dd564c5d312612802d9386f33cd6e97871
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-03-02 10:09:32 -0300

    Update app according to the changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import BlogForm from './components/BlogForm'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(false)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,8 +14,45 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogAppUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const addBlog = blogObject => {
+    blogService.create(blogObject)
+      .then(() => {
+        setBlogs(blogs.concat(blogObject))
+      })
+      .catch(error => {
+        const message = error.message.data
+        console.error(message)
+      })
+  }
+
+  const handleLogout = () => {
+    setUser(null)
+    blogService.setToken(user.token)
+    window.localStorage.removeItem('loggedNoteAppUser')
+  }
+
   return (
     <div>
+      <h1>Wizard's blogs</h1>
+      {
+        user
+          ? <BlogForm
+              addBlog={addBlog}
+              handleLogout={handleLogout}
+            />
+          : <LoginForm
+              handleUser={setUser}
+            />
+      }
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
