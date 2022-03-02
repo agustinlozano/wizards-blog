b2353fb2d8f977f3821eaf2546e98f3beb4e3046
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-03-02 10:08:08 -0300

    Add a component to add blogs

---
+++ src/components/BlogForm.js
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import Toggleable from './Toggleable'
+
+const BlogForm = ({ addBlog, handleLogout }) => {
+  const [newTitle, setNewTitle] = useState()
+  const [newAuthor, setNewAuthor] = useState()
+  const [newUrl, setNewUrl] = useState()
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl
+    }
+
+    addBlog(blogObject)
+    setNewTitle('')
+    setNewAuthor('')
+    setNewUrl('')
+  }
+
+  return (
+    <Toggleable buttonLabel='new blog'>
+      <h3>Create a new blog</h3>
+      <form onSubmit={handleSubmit}>
+        <label hrmlfor='title'>Title</label>
+        <input
+          name='title'
+          placeholder='title'
+          value={newTitle}
+          onChange={({ target }) => setNewTitle(target.value)}
+        />
+        <label hrmlfor='author'>Author</label>
+        <input
+          name='author'
+          placeholder='author'
+          value={newAuthor}
+          onChange={({ target }) => setNewAuthor(target.value)}
+        />
+        <label hrmlfor='url'>Url</label>
+        <input
+          name='url'
+          placeholder='url'
+          value={newUrl}
+          onChange={({ target }) => setNewUrl(target.value)}
+        />
+        <button type='submit'>save</button>
+      </form>
+      <div>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    </Toggleable>
+  )
+}
+
+export default BlogForm
