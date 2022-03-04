aebfe70aba47f80b7fb289c8b84b0891db8e16b0
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-03-04 09:16:20 -0300

    Add a new component to show the user session

---
+++ src/components/UserProfile.js
@@ -0,0 +1,29 @@
+import blogService from '../services/blogs'
+// import propTypes from 'prop-types'
+
+const UserProfile = ({ user, handleUser }) => {
+  const handleLogout = () => {
+    handleUser(null)
+    blogService.setToken(user.token)
+    window.localStorage.removeItem('loggedNoteAppUser')
+  }
+
+  return (
+    <div>
+      <p><b>{user.username}</b> logged in</p>
+      <button
+        onClick={handleLogout}
+        id='btn'
+      >
+        logout
+      </button>
+    </div>
+  )
+}
+
+// UserProfile.propTypes = {
+//   user: propTypes.object.isRequired,
+//   handleUser: propTypes.func.isRequired
+// }
+
+export default UserProfile
