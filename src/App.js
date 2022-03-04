b4fb1adbb662e2a71dfdff012f26eadfab911e93
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-03-04 11:34:44 -0300

    Update app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import blogService from './services/blogs'
-import Blog from './components/Blog'
+import Blogs from './components/Blogs'
 import BlogForm from './components/BlogForm'
 import LoginForm from './components/LoginForm'
 import Notification from './components/Notification'
@@ -81,10 +81,7 @@ const App = () => {
               hanlderNotification={setNotification}
             />
       }
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <Blogs blogs={blogs} />
     </div>
   )
 }
