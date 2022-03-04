b300ce0297e6475dfa8629ac19ea51f4b7d6325c
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-03-04 11:34:11 -0300

    Edit component according to the changes

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,6 +1,27 @@
-const Blog = ({ blog }) =>
-  <div>
-    {blog.title} {blog.author}
-  </div>
+import { useState } from 'react'
 
+const Blog = ({ blog }) => {
+  const [allContent, setAllContent] = useState(false)
+
+  const label = allContent
+    ? 'hide'
+    : '+info'
+
+  return (
+    <div>
+      {
+        allContent
+          ? (
+            <>
+              <b>{blog.title}</b>
+              <p>Author: {blog.author}</p>
+              <p>Link: {blog.url}</p>
+            </>
+            )
+          : <b>{blog.title}</b>
+      }
+      <button onClick={() => setAllContent(!allContent)}>{label}</button>
+    </div>
+  )
+}
 export default Blog
